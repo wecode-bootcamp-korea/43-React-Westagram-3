33f504f28860029147aa97055e0d929937f8014b
ohj1su <ohsj1208@gmail.com>
2023-02-22 17:49:35 +0900

    ADD: 초기셋팅 완료

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        ></>
+        <Route />
       </Routes>
     </BrowserRouter>
   );
