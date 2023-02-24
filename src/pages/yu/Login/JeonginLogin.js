87132e8b79b8336b74f4042a9d6cbfa29554a1bb
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-24 22:24:42 +0900

    Add :Mission 1 - 사용자 입력 데이터 저장 기능 구현,  Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/yu/Login/JeonginLogin.js
+++ src/pages/yu/Login/JeonginLogin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../Login/JeonginLogin.scss';
 
 const JeonginLogin = () => {
   const navigate = useNavigate();
@@ -20,41 +21,43 @@ const JeonginLogin = () => {
 
   return (
     <main className="login">
-      <h1 className="westagram_login">Westagram</h1>
-      <input
-        onChange={saveUserId}
-        value={id}
-        id="LoginId"
-        className="login_id"
-        type="name"
-        placeholder=" 전화번호,
+      <div className="login_box">
+        <h1 className="westagram_login">Westagram</h1>
+        <input
+          onChange={saveUserId}
+          value={id}
+          id="LoginId"
+          className="login_id"
+          type="name"
+          placeholder=" 전화번호,
         사용자이름 또는 이메일"
-        required
-      />
-      <input
-        onChange={saveUserPw}
-        value={pw}
-        id="LoginPw"
-        className="login_pw"
-        type="password"
-        placeholder="
+          required
+        />
+        <input
+          onChange={saveUserPw}
+          value={pw}
+          id="LoginPw"
+          className="login_pw"
+          type="password"
+          placeholder="
         비밀번호"
-        required
-      />
-      <button
-        id="LoginBtn"
-        className={isInputValue ? 'buttonLoginAble' : 'buttonLoginDisable'}
-        disabled={isInputValue ? false : true}
-        onClick={goToMain}
-      >
-        로그인
-      </button>
-      <a
-        className="forgot_pw"
-        href="https://www.instagram.com/accounts/password/reset/"
-      >
-        비밀번호를 잊으셨나요?
-      </a>
+          required
+        />
+        <button
+          id="LoginBtn"
+          className={isInputValue ? 'buttonLoginAble' : 'buttonLoginDisable'}
+          disabled={isInputValue ? false : true}
+          onClick={goToMain}
+        >
+          로그인
+        </button>
+        <a
+          className="forgot_pw"
+          href="https://www.instagram.com/accounts/password/reset/"
+        >
+          비밀번호를 잊으셨나요?
+        </a>
+      </div>
     </main>
   );
 };
