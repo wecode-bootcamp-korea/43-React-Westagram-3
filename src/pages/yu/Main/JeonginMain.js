656bc71abacf58d32177c5c39ac15d0e6c0e8c57
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-01 21:13:53 +0900

    Assignment 1 | Aside에서 상수 데이터 활용

--- src/pages/yu/Main/JeonginMain.js
+++ src/pages/yu/Main/JeonginMain.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useRef } from 'react';
 import JeonginComment from './JeonginComment';
+import INFO_LIST from './jeongin_info';
 import '../../../pages/yu/Main/JeonginMain.scss';
 
 const JeonginMain = () => {
@@ -21,7 +22,6 @@ const JeonginMain = () => {
     setNextId(nextId + 1);
     setComments(newComments);
     setInput('');
-    console.log(comments);
   };
 
   const commentList = comments.map(comment => (
@@ -54,117 +54,98 @@ const JeonginMain = () => {
         </div>
       </div>
 
-      <div className="contents">
-        <div className="feeds">
-          <article>
-            <div className="articleTop">
-              <img className="articleAvatar" src="/images/yu/wecode.jpeg" />
-              <h2>wecode_bootcamp</h2>
-            </div>
-            <img className="articleImage" src="/images/yu/wecode_article.png" />
-            <div className="articleBottom">
-              <div className="buttonBar">
-                <div className="buttonBarLeft" />
-                <div className="buttonBarRight" />
-              </div>
-              <div className="contentsGreat">
-                <img className="contentsAvatar" src="/images/yu/person1.png" />
-                <span>wecode_zzang님 외 4명이 좋아합니다.</span>
-              </div>
-              <span className="userName">wecode_bootcamp</span>
-              <span className="userComment">
-                "위코드는 단순 교육업체가 아닌 개발자 커뮤니티입니다. Wecode에서
-                배우고 저는 총 5개 회사에서 오퍼를 받았습니다."
-              </span>
-              <span className="userMore">...더보기</span>
-              <div className="userTime">54분전</div>
-            </div>
-
-            <div className="addUserComment">
-              <JeonginComment commentList={commentList} />
-            </div>
-            <form className="commentSubmit" onSubmit={submit}>
-              <input
-                className="commentText"
-                type="text"
-                placeholder="댓글 달기..."
-                onChange={onChange}
-                ref={commentInput}
-                value={input}
-              />
-              <button className="commentSubmit">게시</button>
-            </form>
-          </article>
+      <article>
+        <div className="articleTop">
+          <img className="articleAvatar" src="/images/yu/wecode.jpeg" />
+          <h2>wecode_bootcamp</h2>
         </div>
+        <img className="articleImage" src="/images/yu/wecode_article.png" />
+        <div className="articleBottom">
+          <div className="buttonBar">
+            <div className="buttonBarLeft" />
+            <div className="buttonBarRight" />
+          </div>
+          <div className="contentsGreat">
+            <img className="contentsAvatar" src="/images/yu/person1.png" />
+            <span>wecode_zzang님 외 4명이 좋아합니다.</span>
+          </div>
+          <span className="userName">wecode_bootcamp</span>
+          <span className="userComment">
+            "위코드는 단순 교육업체가 아닌 개발자 커뮤니티입니다. Wecode에서
+            배우고 저는 총 5개 회사에서 오퍼를 받았습니다."
+          </span>
+          <span className="userMore">...더보기</span>
+          <div className="userTime">54분전</div>
+        </div>
+        <div className="addUserComment">
+          <JeonginComment commentList={commentList} />
+        </div>
+        <form className="commentSubmit" onSubmit={submit}>
+          <input
+            className="commentText"
+            type="text"
+            placeholder="댓글 달기..."
+            onChange={onChange}
+            ref={commentInput}
+            value={input}
+          />
+          <button className="commentSubmit">게시</button>
+        </form>
+      </article>
 
-        <div className="sideBar">
-          <div className="sideBarTop">
-            <img className="sideBarAvatar" src="/images/yu/person1.png" />
-            <div className="avatarText">
-              <h3 className="name">wecode_bootcamp</h3>
-              <h3 className="nickName">WeCode - 위코드</h3>
+      <div className="aside">
+        <div className="asideTop">
+          <img className="asideAvatar" src="/images/yu/person1.png" />
+          <div className="avatarText">
+            <h3 className="name">wecode_bootcamp</h3>
+            <h3 className="nickName">WeCode - 위코드</h3>
+          </div>
+        </div>
+        <div className="story">
+          <div className="storyTop">
+            <h4 className="storyName">스토리</h4>
+            <h5 className="storyMore">모두 보기</h5>
+          </div>
+          <div className="storyList">
+            <div className="storyPerson">
+              <img className="avatar" src="/images/yu/person1.png" />
+              <div>
+                <div className="avatarId">wecode_zzang</div>
+                <div className="avatarTime">12분 전</div>
+              </div>
             </div>
           </div>
-          <div className="story">
-            <div className="storyTop">
-              <h4 className="storyName">스토리</h4>
-              <h5 className="storyMore">모두 보기</h5>
-            </div>
-            <div className="storyList">
-              <div className="storyPerson">
-                <img className="avatar" src="/images/yu/person1.png" />
+        </div>
+        <div className="recommend">
+          <div className="recommendTop">
+            <h4 className="storyName">회원님을 위한 추천</h4>
+            <h5 className="storyMore">모두 보기</h5>
+          </div>
+          <div>
+            <div className="recommendPerson">
+              <img className="avatar" src="/images/yu/person1.png" />
+              <div className="followPerson">
                 <div>
-                  <div className="avatarId">wecode_zzang</div>
-                  <div className="avatarTime">12분 전</div>
+                  <div className="personId">wecode_zzang</div>
+                  <div className="personTime">12분 전</div>
                 </div>
+                <div className="follow">팔로우</div>
               </div>
             </div>
           </div>
-          <div className="recommend">
-            <div className="recommendTop">
-              <h4 className="storyName">회원님을 위한 추천</h4>
-              <h5 className="storyMore">모두 보기</h5>
-            </div>
-            <div>
-              <div className="recommendPerson">
-                <img className="avatar" src="/images/yu/person1.png" />
-                <div className="followPerson">
-                  <div>
-                    <div className="personId">wecode_zzang</div>
-                    <div className="personTime">12분 전</div>
-                  </div>
-                  <div className="follow">팔로우</div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <ul className="infoPolicy">
-            {INFO_POLICY.map(info => (
-              <span key={info.id} className="infoPolicyList">
-                {info.text}
-              </span>
-            ))}
-            <div className="coperation">2019 WESTAGRAM</div>
-          </ul>
         </div>
+
+        <ul className="infoPolicy">
+          {INFO_LIST.map(info => (
+            <span key={info.id} className="infoPolicyList">
+              {info.text}
+            </span>
+          ))}
+          <div className="coperation">2019 WESTAGRAM</div>
+        </ul>
       </div>
     </div>
   );
 };
 
 export default JeonginMain;
-
-const INFO_POLICY = [
-  { id: 1, text: '소개' },
-  { id: 2, text: '도움말' },
-  { id: 3, text: '홍보 센터' },
-  { id: 4, text: 'API' },
-  { id: 5, text: '채용 정보' },
-  { id: 6, text: '개인정보처리방침' },
-  { id: 7, text: '약관' },
-  { id: 8, text: '위치' },
-  { id: 9, text: '인기 계정' },
-  { id: 10, text: '해시태그' },
-  { id: 11, text: '언어' },
-];
